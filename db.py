979072af8ff485ce41b52d87504fa5017e640b53
Sahla <sahlavalillath@gmail.com>
2025-03-30 21:42:20 +0530

    requirements.txt

--- db.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-from django.conf import settings
-
-client = MongoClient(settings.MONGO_DB_SETTINGS("URI"))
-db = client[setattr.MONGO_DB_SETTINGS["DB_NAME"]]
-
-# db_collection = db["Ecommerce"]
