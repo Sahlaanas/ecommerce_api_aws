8738e312d30233d17a8db9a040583fa8392e0b81
Sahla <sahlavalillath@gmail.com>
2025-03-31 10:39:32 +0530

    error cleared

--- ecommerce_backend/urls.py
+++ ecommerce_backend/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
+sed -i 's/\[]/\["54.206.250.20"]/' /home/ubuntu/Ecommerce_API/ecommerce_backend/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
